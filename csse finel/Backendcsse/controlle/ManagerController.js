76d83a90c7b23ec2f95721344ff8321964e6102f
lahirujay99 <lahirujayawardana94@gmail.com>
2023-10-16 19:13:17 +0530

    added manager routes, model and controller

---
+++ csse finel/Backendcsse/controlle/ManagerController.js
@@ -0,0 +1,32 @@
+const PurchaseOrder = require("../models/PurchaseOrderModel"); // Assuming you have a PurchaseOrder model
+
+// Get all purchase orders
+exports.getAllPurchaseOrders = async (req, res) => {
+  try {
+    const purchaseOrders = await PurchaseOrder.find();
+    res.json(purchaseOrders);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching purchase orders" });
+  }
+};
+
+// Approve a purchase order by ID
+exports.approvePurchaseOrder = async (req, res) => {
+  const orderId = req.params.id;
+
+  try {
+    const purchaseOrder = await PurchaseOrder.findByIdAndUpdate(
+      orderId,
+      { status: "Approved" },
+      { new: true }
+    );
+
+    if (!purchaseOrder) {
+      return res.status(404).json({ error: "Purchase order not found" });
+    }
+
+    res.json(purchaseOrder);
+  } catch (error) {
+    res.status(500).json({ error: "Error approving purchase order" });
+  }
+};
