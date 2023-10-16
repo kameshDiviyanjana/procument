23ffde8f546e1aa348443e94fb7dfd7542443678
umeshdewasinghe <hansithadewasinghe@gmail.com>
2023-10-16 23:08:59 +0530

    supplier

--- csse finel/Backendcsse/models/InvoiceModle.js
+++ csse finel/Backendcsse/models/InvoiceModle.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Invoxesnmodle = new Schema({
+const InvoiceModel = new Schema({
   
-     _id: {type: String, required: true},
-     OrderID : {type: String, required: true},
-     Price:{type: String, required: true},
-     Date :{type: String, required: true},
+    InvoiceID: {type: String, required: true},
+    EmpName: {type: String, required: true},
+    OrderID : {type: String, required: true},
+    Date : {type: String, required: true},
+    Price:{type: Number, required: true},
     Items :{type: String, required: true},
+    Email: {
+        type: String,
+        required: true,
+        match: /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/
+      },
+      
    
 })
 
-const Attendance = mongoose.model("Invice",Invoxesnmodle);
-module.exports = Attendance;
+const Invoice = mongoose.model("Invoice",InvoiceModel);
+module.exports = Invoice;
